4f6ecd7f0fe547a32cac09d4bf5f889fb81cb232
mcalsing <marcelocalsing@gmail.com>
2025-08-21 20:16:17 -0300

    renderizado grana no mapa todo sem o txt.map, video 3

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,6 +1,7 @@
 package main;
 
 import entity.Player;
+import tile.TileManager;
 
 import javax.swing.JPanel;
 import java.awt.*;
@@ -12,22 +13,17 @@ public class GamePanel extends JPanel implements Runnable{
   final int scale = 3;
   
   public final int tileSize = originalTilesSize * scale; //48x48
-  final int maxScreenCol = 16;
-  final int maxScreenRow = 12;
-  final int screenWidth = tileSize * maxScreenCol; // 768px
-  final int screenHeight = tileSize * maxScreenRow; // 576px
+  public final int maxScreenCol = 16;
+  public final int maxScreenRow = 12;
+  public final int screenWidth = tileSize * maxScreenCol; // 768px
+  public final int screenHeight = tileSize * maxScreenRow; // 576px
 
   int FPS = 60;
 
+  TileManager tileM = new TileManager(this);
   KeyHandler keyH = new KeyHandler();
   Thread gameThread;
   Player player = new Player(this,keyH);
-
-
-  // Set players default position
-  int playerX = 100;
-  int playerY = 100;
-  int playerSpeed = 4;
 
   public GamePanel() {
     this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -49,7 +45,7 @@ public class GamePanel extends JPanel implements Runnable{
   //Função Loop
   @Override
   public void run() {
-    double drawInterval = 1000000000/FPS; // 0.01666 seconds
+    double drawInterval = (double) 1000000000/FPS; // 0.01666 seconds
     double delta = 0;
     long lastTime = System.nanoTime();
     long currentTime;
@@ -86,6 +82,8 @@ public class GamePanel extends JPanel implements Runnable{
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
     Graphics2D g2 = (Graphics2D)g;
+
+    tileM.draw(g2);
 
     player.draw(g2);
 
