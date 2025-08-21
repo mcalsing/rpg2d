45d42df44be3c02ef7f3710899a0343e84eec8d9
mcalsing <marcelocalsing@gmail.com>
2025-08-21 13:08:53 -0300

    definido dimensões dos SQMs e da janela do jogo, video 1

---
+++ src/main/Main.java
@@ -0,0 +1,26 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+  public static void main(String[] args) {
+
+    // Configurações de exibição da tela
+    JFrame window = new JFrame();
+    window.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
+    window.setResizable(false);
+    window.setTitle("RPG 2D");
+
+    GamePanel gamePanel = new GamePanel();
+    window.add(gamePanel);
+
+    window.pack();
+
+    window.setLocationRelativeTo(null);
+    window.setVisible(true);
+
+    gamePanel.startGameThread();
+
+
+  }
+}
