bc93fd94382a2466f7c79ae987bb6f5e39eb6dcf
mcalsing <marcelocalsing@gmail.com>
2025-08-21 21:35:23 -0300

    mapa do jogo sendo renderizado por arquivo .txt, video 3

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -4,17 +4,23 @@ import main.GamePanel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.Objects;
 
 public class TileManager {
 
   GamePanel gp;
   Tile[] tile;
+  int[][] mapTileNum;
 
   public TileManager(GamePanel gp) {
     this.gp = gp;
     tile = new Tile[10];
+    mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
+
     getTileImage();
+    loadMap("res/maps/map01.txt");
   }
 
   public void getTileImage() {
@@ -33,6 +39,32 @@ public class TileManager {
     }
   }
 
+  public void loadMap(String filePath) {
+    try {
+      //String map01 = "res/maps/map01.txt";
+      FileReader is = new FileReader(filePath);
+      BufferedReader br = new BufferedReader(is);
+
+      int col = 0;
+      int row = 0;
+      String line;
+
+      while ((line = br.readLine())!=null && row<gp.maxScreenRow) {
+        //System.out.println("linea :"+row+"-"+line);
+
+        String[] numbers = line.split(" ");
+
+        for (col=0;col<gp.maxScreenCol;col++) {
+          mapTileNum[col][row]=Integer.parseInt(numbers[col]);
+        }
+        row++;
+      }
+
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
   public void draw(Graphics g2) {
 
     int col = 0;
@@ -42,20 +74,17 @@ public class TileManager {
 
     while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
 
-      try {
-        g2.drawImage(tile[0].image, x, y, gp.tileSize, gp.tileSize, null);
-        col++;
-        x += gp.tileSize;
+      int tileNum = mapTileNum[col][row];
 
-        if (col == gp.maxScreenCol) {
-          col = 0;
-          x = 0;
-          row++;
-          y += gp.tileSize;
-        }
+      g2.drawImage(tile[tileNum].image, x, y, gp.tileSize, gp.tileSize, null);
+      col++;
+      x += gp.tileSize;
 
-      } catch (Exception e) {
-        throw new RuntimeException(e);
+      if (col == gp.maxScreenCol) {
+        col = 0;
+        x = 0;
+        row++;
+        y += gp.tileSize;
       }
     }
 
